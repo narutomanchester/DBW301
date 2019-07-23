2956b2462ce3831739ab4dbe4cdedc32b7a07c9b
hiephh-teko <hiep.hh@teko.vn>
2019-07-23 09:35:13 +0700

    commit full script gen DB

--- DBW301/models/gen_db.py
+++ DBW301/models/gen_db.py
@@ -17,6 +17,12 @@ class GenDB(object):
         month = randint(1,12)
         date = randint(1,28)
         return "%s-%s-%s"%(str(year),str(month),str(date))
+    
+    def random_phone_number(self):
+        S = ""
+        for x in range(10):
+            S += "%s"%(randint(0,9))
+        return S
 
     def gen_customer_table(self):
         for x in range(10000):
@@ -53,7 +59,55 @@ class GenDB(object):
             db.session.add(headquarter)
         db.session.commit()
 
+    def gen_stores_table(self):
+        for x in range(10000):
+            x+=1
+            stores = Stores(store_id=x,city_id=x,phone=self.random_phone_number(),start_time=self.random_date())
+            db.session.add(stores)
+        db.session.commit()
+    
+    def gen_items_table(self):
+        for x in range(10000):
+            x+=1
+            description = "this is description %s"%(str(x))
+            size = randrange(20)
+            weight = randrange(70)
+            unit_price = randrange(100)
+            items = Items(item_id=x,description=description,size=size,weight=weight,unit_price=unit_price,manufacturing_time=self.random_date())
+            db.session.add(items)
+        db.session.commit()
+
+    def gen_stored_items_table(self):
+        for x in range(10000):
+            x+=1
+            quantity_held = randrange(100)
+            stored_items = StoredItem(store_id=x,item_id=x,quantity_held=quantity_held,time_in=self.random_date())
+            db.session.add(stored_items)
+        db.session.commit()
+        
+    def gen_order_table(self):
+        for x in range(10000):
+            x+=1
+           
+            order = Order(order_no=x,customer_id=x,order_date=self.random_date())
+            db.session.add(order)
+        db.session.commit()
+    
+    def gen_ordered_item_table(self):
+        for x in range(10000):
+            x+=1
+            quantity_ordered = randrange(15)
+            ordered_price = randrange(100)
+            ordered_item = OrderedItem(order_no=x,item_id=x,quantity_ordered=quantity_ordered,ordered_price=ordered_price,time_order=self.random_date())
+            db.session.add(ordered_item)
+        db.session.commit()
+
 GenDB().gen_customer_table()
 GenDB().gen_walk_in_customer_table()
 GenDB().gen_mail_order_customer_table()
 GenDB().gen_headquater_table()
+GenDB().gen_stores_table()
+GenDB().gen_items_table()
+GenDB().gen_stored_items_table()
+GenDB().gen_order_table()
+GenDB().gen_ordered_item_table()
